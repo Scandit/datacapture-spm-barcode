f188b766850039648f307f29491ef04051a9be03
Scandit <scandit@juicy.int.mirasense.com>
2021-06-03 21:57:09 +0200

    Updating ScanditBarcodeCapture - v.6.8.0

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ let package = Package(
     ],
     dependencies: [],
     targets: [
-        .binaryTarget(name: "ScanditBarcodeCapture", url: "https://ssl.scandit.com/sdk/download/scandit-datacapture-ios-barcode-6.8.0-beta.2-xcframework.zip", checksum: "6e54c2aa57d6fb85c9d44faedacaaeaa749ce33e394a3877c3cb0878c5a7ff5e")
+        .binaryTarget(name: "ScanditBarcodeCapture", url: "https://ssl.scandit.com/sdk/download/scandit-datacapture-ios-barcode-6.8.0-xcframework.zip", checksum: "945dcca49b00cf56c8d7e393aa1233f2d7ab78d38761a4fb6c8a6acafcdfaaa6")
     ]
 )
